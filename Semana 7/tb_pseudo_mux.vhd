a4556ad21dc8aa1b4b3fc2abb40be5b537cc2d37
Daniel Alves <danielresende2000@gmail.com>
2022-06-11 17:54:09 -0300

    ✨ Implementação do testbench

--- Semana 7/tb_pseudo_mux.vhd
+++ Semana 7/tb_pseudo_mux.vhd
@@ -15,22 +15,78 @@ component pseudo_mux is
     );
 end component;
 
+
+signal CLOCK_50MHz : std_logic;
+signal buttonPress : std_logic := '0';
+signal inputA, inputB, inputC, inputD : std_logic := '0';
+signal output : std_logic := '0';
 -- Clock period definitions
-   constant PERIOD     : time := 20 ns;
-   constant DUTY_CYCLE : real := 0.5;
-   constant OFFSET     : time := 5 ns;
+constant PERIOD     : time := 10 ns;
+constant DUTY_CYCLE : real := 0.5;
+constant OFFSET     : time := 5 ns;
+
+begin
+    instancia_pseudo_mux : pseudo_mux port map(CLOCK=>CLOCK_50MHz, S=>buttonPress, A=>inputA, B=>inputB, C=>inputC, D=>inputD, Q=>output);
+
+    clock_manager: process    -- clock process for clock
+    begin
+        wait for OFFSET;
+        CLOCK_LOOP : loop
+            CLOCK_50MHz <= '0';
+            wait for (PERIOD - (PERIOD * DUTY_CYCLE));
+            CLOCK_50MHz <= '1';
+            wait for (PERIOD * DUTY_CYCLE);
+        end loop CLOCK_LOOP;
+    end process clock_manager;
+    
+    library ieee;
+use ieee.std_logic_1164.all;
+
+entity tb_pseudo_mux is
+end tb_pseudo_mux;
+
+architecture teste of tb_pseudo_mux is
+
+component pseudo_mux is
+	port (
+        CLOCK   : in    std_logic; -- clock input
+        S       : in    std_logic; -- control input
+        A,B,C,D : in    std_logic; -- data inputs
+        Q       : out   std_logic  -- data output
+    );
+end component;
 
 
----------------------------------------------------
-------- process para gerar o sinal de clock -------
----------------------------------------------------
-        PROCESS(OFFSET)    -- clock process for clock
-        BEGIN
-            WAIT for OFFSET;
-            CLOCK_LOOP : LOOP
-                CLOCK_50MHz <= '0';
-                WAIT FOR (PERIOD - (PERIOD * DUTY_CYCLE));
-                CLOCK_50MHz <= '1';
-                WAIT FOR (PERIOD * DUTY_CYCLE);
-            END LOOP CLOCK_LOOP;
-        END PROCESS;
+signal CLOCK_50MHz : std_logic := '0';
+signal buttonPress : std_logic := '0';
+signal inputA, inputB, inputC, inputD : std_logic := '0';
+signal output : std_logic := '0';
+-- Clock period definitions
+constant PERIOD     : time := 10 ns;
+constant DUTY_CYCLE : real := 0.5;
+constant OFFSET     : time := 5 ns;
+
+begin
+    instancia_pseudo_mux : pseudo_mux port map(CLOCK=>CLOCK_50MHz, S=>buttonPress, A=>inputA, B=>inputB, C=>inputC, D=>inputD, Q=>output);
+
+    clock_manager: process    -- clock process for clock
+    begin
+        wait for OFFSET;
+        CLOCK_LOOP : loop
+            CLOCK_50MHz <= '0';
+            wait for (PERIOD - (PERIOD * DUTY_CYCLE));
+            CLOCK_50MHz <= '1';
+            wait for (PERIOD * DUTY_CYCLE);
+        end loop CLOCK_LOOP;
+    end process clock_manager;
+    
+    inputA <= '1', '0' after 50 ns;
+    inputB <= '0', '1' after 76 ns, '0' after 150 ns;
+    inputC <= '0', '1' after 176 ns, '0' after 250 ns;
+    inputD <= '0', '1' after 276 ns, '0' after 350 ns;
+    buttonPress <= not(buttonPress) after 50 ns;
+
+end teste;
+
+
+end teste;
