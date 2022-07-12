13ab282b6725ddc349a006ab9ca8d088b67d4c42
Daniel Alves <danielresende2000@gmail.com>
2022-07-12 16:03:48 -0300

    separando blocos de memória

---
+++ Trabalho Final/EEPROM.vhd
@@ -0,0 +1,37 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+-- MEMÓRIA EEPROM
+entity EEPROM is
+	generic (
+		ADDR_LENGHT	: natural := 4;
+		R_LENGHT	: natural := 8;
+		NUM_OF_REGS	: natural := 16
+	);
+	port (
+		clk 	: in std_logic
+		wr 		: in std_logic
+		addr 	: in std_logic_vector (ADDR_LENGHT - 1 downto 0);
+		datain 	: in std_logic_vector (R_LENGHT - 1 downto 0);
+		dataout : out std_logic_vector (R_LENGHT - 1 downto 0);
+	);
+end entity;
+
+architecture ROM_REGISTER of EEPROM is
+	type rom_reg is array (0 to NUM_OF_REGS - 1) of
+			std_logic_vector (R_LENGHT - 1 downto 0);
+	signal rom_s : rom_reg;
+
+	process(clk)
+	variable loc : integer;
+	begin
+		if (rising_edge(clk)) then
+			loc := to_integer(unsigned(addr));
+			if (wr = '1') then
+				rom_s(loc) <= datain;
+			end if;
+			dataout <= ram_s(loc);
+		end if;
+	end process;
+end architecture ROM_REGISTER;
---
+++ Trabalho Final/flipflop_rs.vhd
@@ -0,0 +1,24 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+-- FLIP FLOP RS
+entity flip_flop_rs is
+	port (
+		clk : in std_logic;
+		set : in std_logic;
+		rst : in std_logic;
+		saida : out std_logic;
+	);
+end entity flip_flop_rs;
+
+architecture flip of flip_flop_rs is
+	begin
+		process (clk, rst) is
+		begin
+			if(rst = '0') then
+				saida <= '0';
+			elsif (rising_edge(clk) and (set = '1')) then
+				saida <= '1';
+			end if;
+		end process;
+end architecture flip;
