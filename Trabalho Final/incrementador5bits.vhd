1823b11fcfe90e361d2bfbe6e55849c3da57840e
Johnatan Augusto <augustosales2010@gmail.com>
2022-07-13 23:49:07 -0300

    testbench incrementador GRAÇAS a DEUS

--- Trabalho Final/incrementador5bits.vhd
+++ Trabalho Final/incrementador5bits.vhd
@@ -3,7 +3,10 @@ USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 
 ENTITY incrementador5bits IS
-	GENERIC (n : NATURAL := 5);
+	GENERIC (
+		n : NATURAL := 5;
+		one : STD_LOGIC_VECTOR(n - 1 DOWNTO 0) := "00001"
+	);
 	PORT (
 		Number : IN STD_LOGIC_VECTOR(n - 1 DOWNTO 0);
 		OutputNumber : OUT STD_LOGIC_VECTOR(n - 1 DOWNTO 0)
@@ -12,5 +15,5 @@ END ENTITY;
 
 ARCHITECTURE sum OF incrementador5bits IS
 BEGIN
-	OutputNumber <= STD_LOGIC_VECTOR(unsigned(FirstNumber) + unsigned('1'));
+	OutputNumber <= STD_LOGIC_VECTOR(unsigned(Number) + unsigned(one));
 END sum;
--- Trabalho Final/tb_incrementador5bits.vhd
+++ Trabalho Final/tb_incrementador5bits.vhd
@@ -8,20 +8,22 @@ END tb_incrementador5bits;
 ARCHITECTURE testeIncrementador5bits OF tb_incrementador5bits IS
 
 	COMPONENT incrementador5bits IS
-		GENERIC (n : NATURAL := 16);
+		GENERIC (
+			n : NATURAL := 5;
+			one : STD_LOGIC_VECTOR(n - 1 DOWNTO 0) := "00001"
+		);
 		PORT (
 			Number : IN STD_LOGIC_VECTOR(n - 1 DOWNTO 0);
 			OutputNumber : OUT STD_LOGIC_VECTOR(n - 1 DOWNTO 0)
 		);
 	END COMPONENT;
 
-	SIGNAL NUM : STD_LOGIC_VECTOR(15 DOWNTO 0);
-	SIGNAL OUT : STD_LOGIC_VECTOR(15 DOWNTO 0);
+	SIGNAL NUM : STD_LOGIC_VECTOR(4 DOWNTO 0) := "00000";
+	SIGNAL OutNumber : STD_LOGIC_VECTOR(4 DOWNTO 0);
 
 BEGIN
-	instancia_Incrementador5bits : incrementador5bits PORT MAP(Number => NUM, OutputNumber => OUT);
+	instancia_Incrementador5bits : incrementador5bits PORT MAP(Number => NUM, OutputNumber => OutNumber);
 
-	NUM <= STD_LOGIC_VECTOR(to_unsigned(1, 16)),
-		STD_LOGIC_VECTOR(to_unsigned(5, 16)) AFTER 50 ns;
+	NUM <= STD_LOGIC_VECTOR(unsigned(NUM) + to_unsigned(1, 5)) AFTER 50 ns;
 
 END testeIncrementador5bits;
