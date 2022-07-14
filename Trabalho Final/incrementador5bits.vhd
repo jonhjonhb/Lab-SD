51f484b23e5d7259f74f086a23972375cc981d3e
Daniel Alves <danielresende2000@gmail.com>
2022-07-13 22:28:15 -0300

    adicionando incrementador

--- Trabalho Final/incrementador5bits.vhd
+++ Trabalho Final/incrementador5bits.vhd
@@ -2,7 +2,7 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 
-ENTITY somador5bits IS
+ENTITY incrementador5bits IS
 	GENERIC (n : NATURAL := 5);
 	PORT (
 		Number : IN STD_LOGIC_VECTOR(n - 1 DOWNTO 0);
@@ -10,7 +10,7 @@ ENTITY somador5bits IS
 	);
 END ENTITY;
 
-ARCHITECTURE sum OF somador16bits IS
+ARCHITECTURE sum OF incrementador5bits IS
 BEGIN
-	OutputNumber <= STD_LOGIC_VECTOR(unsigned(FirstNumber) + unsigned(SecondNumber));
+	OutputNumber <= STD_LOGIC_VECTOR(unsigned(FirstNumber) + unsigned('1'));
 END sum;
